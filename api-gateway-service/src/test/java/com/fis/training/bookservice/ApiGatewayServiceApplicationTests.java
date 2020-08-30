8216941f7f07ccfaa600113e3f9cfe851f615524
amitgarg196 <amitgarg.196@gmail.com>
2020-08-30 21:25:00 +0530

    Add files via upload

---
+++ api-gateway-service/src/test/java/com/fis/training/bookservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.training.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
